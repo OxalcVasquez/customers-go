4fce7cf7851c11711586667295fdf5edb58ef9c1
oxalcvasquez <oxalc0101@gmail.com>
2024-04-22 21:38:16 -0500

    Customer repository implement

---
+++ repository/customer_repository.go
@@ -0,0 +1,75 @@
+package repository
+
+import (
+	"customers-api/domain/dao"
+
+	log "github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+)
+
+// Interfaz
+type CustomerRepository interface {
+	FindAllCustomer() ([]dao.Customer, error)
+	FindCustomerById(id int) (dao.Customer, error)
+	Save(customer *dao.Customer) (dao.Customer, error)
+	Update(id int, customer *dao.Customer) (dao.Customer, error)
+	Delete(id int) error
+}
+
+// Implementacion
+type CustomerRepositoryImpl struct {
+	db *gorm.DB
+}
+
+// Inicializacion
+func CustomerRepositoryInit(db *gorm.DB) *CustomerRepositoryImpl {
+	db.AutoMigrate(&dao.Customer{})
+	return &CustomerRepositoryImpl{
+		db: db,
+	}
+}
+
+func (customerRepo CustomerRepositoryImpl) FindAllCustomer() ([]dao.Customer, error) {
+	var customers []dao.Customer
+
+	var err = customerRepo.db.Preload("type").Find(&customers).Error
+	if err != nil {
+		log.Error("Error finding all customers. Error: ", err)
+		return nil, err
+	}
+
+	return customers, nil
+}
+
+func (customerRepo CustomerRepositoryImpl) FindCustomerById(id int) (dao.Customer, error) {
+	customer := dao.Customer{
+		ID: id,
+	}
+
+	err := customerRepo.db.Preload("Type").First(&customer).Error
+	if err != nil {
+		log.Error("Error finding customer by id. Error", err)
+		return dao.Customer{}, err
+	}
+
+	return customer, nil
+
+}
+
+func (customerRepo CustomerRepositoryImpl) Save(customer *dao.Customer) (dao.Customer, error) {
+	var err = customerRepo.db.Save(customer).Error
+	if err != nil {
+		log.Error("Error saving customer by id. Error", err)
+		return dao.Customer{}, err
+	}
+	return *customer, nil
+}
+
+func (customerRepo CustomerRepositoryImpl) DeleteCustomerById(id int) error {
+	err := customerRepo.db.Delete(&dao.Customer{}, id).Error
+	if err != nil {
+		log.Error("Error deleting customer. Error: ", err)
+		return err
+	}
+	return nil
+}
