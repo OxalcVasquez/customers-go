18ce674dfc672c00df792d42ce582e7bc6a269a5
oxalcvasquez <oxalc0101@gmail.com>
2024-04-22 23:50:21 -0500

    Main configuration

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"customers-api/config"
+	"customers-api/router"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	godotenv.Load()
+	config.InitLog()
+}
+
+func main() {
+	port := os.Getenv("PORT")
+
+	init := config.Init()
+	app := router.Init(init)
+
+	app.Run(":" + port)
+}
